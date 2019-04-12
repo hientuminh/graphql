1320ab78c727f751379f5c117cfd77956edd7d28
hientuminh <hientuminh@gmail.com>
2019-04-12 17:23:45 +0700

    4-authentication: Add createUser and SignIn for mutation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,8 +6,13 @@ module Types
     # TODO: remove me
     field :test_field, String, null: false, description: "An example field added by the generator"
     field :all_links, [LinkType], null: false
+    field :all_users, [UserType], null: false
     def all_links
       Link.all
+    end
+
+    def all_users
+      User.all
     end
   end
 end
